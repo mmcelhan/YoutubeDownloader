776218192ebcd8090cae341bc6e26a33ddc526ed
mmcelhan <matt@lamplightlab.com>
2025-04-16 17:44:09 -0600

    push

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-from pytube import YouTube
+from pytubefix import YouTube
 import os
 
 
@@ -16,6 +16,13 @@ def initialize():
     return None
 
 
+# little utility to parse out the resolution from a stream description like 1440p
+def get_resolution(s):
+    return int(s.resolution[:-1])
+
+
+
+
 def file_scrub(text_input):
     # removes periods and commas from a file name
     text_input = text_input.replace(',', '').replace('.', '')
@@ -25,8 +32,17 @@ def file_scrub(text_input):
 def youtube_dl(download_path, download_type='Video', link='https://www.youtube.com/watch?v=TK4N5W22Gts'):
     if download_type == 'Video':
         yt = YouTube(link)
-        video = yt.streams.filter(progressive=True).last()  # get highest resolution
-        video.download(download_path)
+        try:
+            stream = max(
+                filter(lambda s: get_resolution(s) <= 1080,  # filter out sub-1080p streams
+                       filter(lambda s: s.type == 'video', yt.fmt_streams)),  # filter out the video streams
+                key=get_resolution  # maximum resolution among those streams
+            )
+
+            stream.download(download_path)
+        except:
+            ys = yt.streams.get_highest_resolution()
+            ys.download(download_path)
 
     else:
         yt = YouTube(link)
@@ -36,9 +52,15 @@ def youtube_dl(download_path, download_type='Video', link='https://www.youtube.c
         audio_root_name = yt.title
 
         # rename the file to mp3
-        audio_file_name = str(file_scrub(audio_root_name)) + '.mp4'
+        original_filename = os.path.join(download_path, audio_root_name) + '.m4a'
+        audio_file_name = os.path.join(download_path, str(file_scrub(audio_root_name))) + '.mp3'
+        os.rename(original_filename, audio_file_name)
+
+        """
+        # rename the file to mp3
+        audio_file_name = str(file_scrub(audio_root_name)) + '.m4a'
         original_filename = os.path.join(download_path, audio_file_name)
         new_filename = os.path.join(download_path, audio_root_name + '.mp3')
         os.rename(original_filename, new_filename)
-
+        """
     return None  # end function
